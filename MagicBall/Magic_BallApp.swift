202dd248662a7b82c8d038964f46afa77cc76bb2
Nikita Shvadlenko <75701351+NikitaShvadlenko@users.noreply.github.com>
2022-10-10 11:01:13 +1300

    Add files via upload

---
+++ MagicBall/Magic_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_BallApp.swift
+//  Magic Ball
+//
+//  Created by Nikita Shvad on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MagicBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
